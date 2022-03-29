7088a465de5e1eb46d253416eb6aebc68ce60c82
cyclones <enes.cyclones@gmail.com>
2022-03-29 21:52:50 +0300

    İnitial Commit

---
+++ server/src/test/java/com/duzce/mf/vtys/rent/a/car/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duzce.mf.vtys.rent.a.car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
