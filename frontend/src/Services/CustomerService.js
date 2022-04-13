5fe854f1ec6043bda42d7afc6b6680f9d22ff9c2
Enes Şahin <enes.cyclones@gmail.com>
2022-04-13 23:13:58 +0300

    Next Js App Created

--- frontend/src/Services/CustomerService.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export class CustomerService{
-
-    getAllCustomers(){
-        return axios.get("http://localhost:8080/api/v1/customer");
-    }
-}
--- server/src/main/java/com/duzce/vtys/rentacar/controller/DriverLicenceController.java
+++ server/src/main/java/com/duzce/vtys/rentacar/controller/DriverLicenceController.java
@@ -7,14 +7,12 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import oracle.jdbc.proxy.annotation.Post;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/v1/driverlicence")
 public class DriverLicenceController {
 
